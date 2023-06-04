60ff7769dd8e1f47e6f47c95781e1b2312e2d78c
dinhthanhhai <dthk164@gmail.com>
2023-06-04 16:26:52 +0700

    cau hinh viewengine

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-const express = require('express')//import thu vien express
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+
 const app = express()//tao app de su dung tat ca ham cua express
 const port = 3000//tao cong
 
+configViewEngine(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index.ejs')
 })
 
 app.get('/about', (req, res) => {
-    res.send(`I'm Hai`)
+    res.send(`Dinh Thanh Hai`)
 })
 
 
