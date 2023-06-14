cad95f3c05ce0056939c18ae65d0a47e5ddd9333
dinhthanhhai <dthk164@gmail.com>
2023-06-14 23:21:02 +0700

    post request-add user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -11,7 +11,14 @@ let getDetailPage = async (req, res) => {
     let user = await pool.execute('SELECT * from users where id = ?', [id])
     return res.send(JSON.stringify(user[0]));
 }
+let createNewUser = async (req, res) => {
+    console.log('>>>checkreq: ', req.body);
+    let { firstName, lastName, email, address } = req.body;
+    await pool.execute('INSERT INTO users(firstName, lastName, email, address) VALUES (?, ?, ?, ?)',
+        [firstName, lastName, email, address]);
+    return res.redirect('/');//ve trang home khi submit
+}
 
 module.exports = {
-    getHomePage, getDetailPage
+    getHomePage, getDetailPage, createNewUser
 }
--- src/server.js
+++ src/server.js
@@ -4,8 +4,14 @@ import initWebroute from './route/web'
 import connection from './configs/connectDB'
 require('dotenv').config();
 
+
+
+
 const app = express()//tao app de su dung tat ca ham cua express
 const port = process.env.PORT || 3000;//neu process.env.PORT: undefined thi dung cong 3000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 configViewEngine(app);
 
